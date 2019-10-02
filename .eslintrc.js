a524bed1ebf54ea287cfb8d93d5499b7bc29e4fe
cao <kum_cp@yahoo.com.vn>
2019-10-02 16:35:04 +0900

    fix indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,10 @@ module.exports = {
     },
     extends: ['plugin:vue/essential', '@vue/airbnb', 'plugin:prettier/recommended'],
     rules: {
+        'prettier/prettier': 'error',
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4]
     },
     parserOptions: {
         parser: 'babel-eslint',
